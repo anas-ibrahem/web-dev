b2b1f84de8b4509b8c23ea4493afb6fe871eb8a8
anasibrahem <anasibrahem101@gmail.com>
2024-08-03 17:50:36 +0300

    🌃 Writing Functions

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -2,15 +2,67 @@ const letters = document.querySelectorAll(".game-letter");
 const menu = document.querySelector(".start-menu");
 let mode = 0;
 let guessNumber = 0;
-const sMenu = document.getElementById('start-menu'); // Start Menu
-const lMenu = document.getElementById('lose-menu');  // Losing Menu
-const wMenu = document.getElementById('win-menu');   // Winning Menu
-
+let buffer = "";
+const sMenu = document.getElementById("start-menu"); // Start Menu
+const lMenu = document.getElementById("lose-menu"); // Losing Menu
+const wMenu = document.getElementById("win-menu"); // Winning Menu
+const WORD_LENGTH = 5; // Winning Menu
 
 function startGame(choice) {
-   sMenu.classList.add("disable");
-    mode = choice;
-    guessNumber = 0;
-}       
+  sMenu.classList.add("disable");
+  mode = choice;
+  reset();
+  init();
+}
 
-console.log(letters[0].innerText);
+function reset() {
+  guessNumber = 0;
+  buffer = "";
+}
+
+function init() {
+  document.addEventListener("keydown", function handleKeyPress(event) {
+    const action = event.key;
+
+    if (action === "Enter") {
+      submit();
+    } else if (action === "Backspace") {
+      backSpace();
+    } else if (isLetter(action)) {
+      addLetter(action.toUpperCase());
+    } else {
+      // do nothing
+    }
+  });
+
+  function addLetter(letter) {
+    if (buffer.length === WORD_LENGTH)
+      buffer = buffer.substring(0, WORD_LENGTH - 1);
+    buffer += letter;
+    letters[guessNumber * WORD_LENGTH + buffer.length - 1].innerText = letter;
+  }
+
+  function backSpace(){
+    buffer = buffer.substring(0, buffer.length - 1);
+    letters[guessNumber * WORD_LENGTH + buffer.length ].innerText = '';
+  }
+
+  function submit(){
+    if(buffer.length < WORD_LENGTH)
+    {
+        // do nothing
+    }
+    else
+    {
+
+
+
+
+    }
+
+  }
+}
+
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
+}
